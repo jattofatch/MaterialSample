1100f1f4c68fbecf52876234ce6c58323f77e857
jattofatch <jattofatch@gmail.com>
2017-09-12 09:14:07 +0200

    First commit
    just simple materialize stuff

---
+++ js/trial1.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    //$('body').hide().fadeIn();
+    $('#gg').hide().fadeIn(3000);
+    var book = $('.dove').html();
+    var books =$('#gg').attr('name');
+    $('#gg').text('i am really doing ths');
+    //$('#p1').html('<b>get him b</b>');
+
+
+});
